5d24fc7d64db13076c5ee4fdab4dd79489f5e991
Pedro Henrique Mendonça <pedroh36mendonca@gmail.com>
2025-10-22 17:34:01 -0300

    Mudança do Url base

--- app/src/main/java/com/example/myapplication/remote/RetrofitClient.kt
+++ app/src/main/java/com/example/myapplication/remote/RetrofitClient.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitClient {
     // URL base da sua API.
     // 10.0.2.2 é o endereço especial que o emulador Android usa para se conectar ao localhost do seu computador.
-    private const val BASE_URL = "http://192.168.0.172:8080/"
+    private const val BASE_URL = "https://app-tcc-backend.onrender.com/"
 
     // Instância única e "preguiçosa" (lazy) do Retrofit.
     // Ela só será criada na primeira vez que for usada.
